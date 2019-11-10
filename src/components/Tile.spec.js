77b0d3de74cac262c9e481d4a9851c104831b435
kfode <kfode@sourceallies.com>
2019-11-10 14:00:59 -0600

    Added missing test.

--- src/components/Tile.spec.js
+++ src/components/Tile.spec.js
@@ -3,7 +3,7 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Chance from 'chance';
 import Tile from './Tile';
-import { tileTypes } from '../enums';
+import { tileTypes, dieColor, dieFace } from '../enums';
 
 const chance = new Chance();
 
@@ -30,7 +30,7 @@ describe('Tile', () => {
     it('should render a blue world tile', () => {
         expectedTileProps.tileType = tileTypes.BLUE_WORLD;
 
-        const {getByTestId} = render(<Tile {...expectedTileProps} />);
+        const { getByTestId } = render(<Tile {...expectedTileProps} />);
 
         expect(getByTestId('blue-world')).toBeTruthy();
     });
@@ -38,7 +38,7 @@ describe('Tile', () => {
     it('should render a brown world tile', () => {
         expectedTileProps.tileType = tileTypes.BROWN_WORLD;
 
-        const {getByTestId} = render(<Tile {...expectedTileProps} />);
+        const { getByTestId } = render(<Tile {...expectedTileProps} />);
 
         expect(getByTestId('brown-world')).toBeTruthy();
     });
@@ -46,7 +46,7 @@ describe('Tile', () => {
     it('should render a development tile', () => {
         expectedTileProps.tileType = tileTypes.DEVELOPMENT;
 
-        const {getByTestId} = render(<Tile {...expectedTileProps} />);
+        const { getByTestId } = render(<Tile {...expectedTileProps} />);
 
         expect(getByTestId('development')).toBeTruthy();
     });
@@ -54,7 +54,7 @@ describe('Tile', () => {
     it('should render a gray world tile', () => {
         expectedTileProps.tileType = tileTypes.GRAY_WORLD;
 
-        const {getByTestId} = render(<Tile {...expectedTileProps} />);
+        const { getByTestId } = render(<Tile {...expectedTileProps} />);
 
         expect(getByTestId('gray-world')).toBeTruthy();
     });
@@ -62,7 +62,7 @@ describe('Tile', () => {
     it('should render a green world tile', () => {
         expectedTileProps.tileType = tileTypes.GREEN_WORLD;
 
-        const {getByTestId} = render(<Tile {...expectedTileProps} />);
+        const { getByTestId } = render(<Tile {...expectedTileProps} />);
 
         expect(getByTestId('green-world')).toBeTruthy();
     });
@@ -70,7 +70,7 @@ describe('Tile', () => {
     it('should render a yellow world tile', () => {
         expectedTileProps.tileType = tileTypes.YELLOW_WORLD;
 
-        const {getByTestId} = render(<Tile {...expectedTileProps} />);
+        const { getByTestId } = render(<Tile {...expectedTileProps} />);
 
         expect(getByTestId('yellow-world')).toBeTruthy();
     });
@@ -79,8 +79,23 @@ describe('Tile', () => {
         expectedTileProps.name = 'Broken Tile';
         expectedTileProps.tileType = 'WAT';
 
-        const {getByText} = render(<Tile {...expectedTileProps} />);
+        const { getByText } = render(<Tile {...expectedTileProps} />);
 
         expect(getByText('Invalid tile type: WAT for tile: Broken Tile')).toBeTruthy();
+    });
+
+    it('should render a die when a dicePool is provided', () => {
+        expectedTileProps.dicePool = {
+            dice: [
+                {
+                    color: dieColor.WHITE,
+                    face: dieFace.EXPLORE
+                }
+            ]
+        };
+
+        const { getByTestId } = render(<Tile {...expectedTileProps} />);
+
+        expect(getByTestId('WhiteDie')).toBeTruthy();
     });
 });
