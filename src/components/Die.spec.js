dfefed3f36da1e5e54ea4e3ab5200e6436bb34d1
kfode <kfode@sourceallies.com>
2019-12-03 16:13:46 -0600

    Added die tests for dragging.

--- src/components/Die.spec.js
+++ src/components/Die.spec.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Die from './Die';
 import { dieColor, dieFace } from '../enums';
 import { getMockDie } from '../test-utilities/mock-object-generators';
 
 describe('Die', () => {
-    let expectedDieProps;
+    let expectedDieProps, mockOnDragStart, mockOnDragStartCalled = false;
+
+    mockOnDragStart = (event, id) => {
+        mockOnDragStartCalled = true;
+    }
 
     beforeEach(() => {
         expectedDieProps = getMockDie();
+        mockOnDragStartCalled = false
     });
 
     afterEach(cleanup);
@@ -128,5 +133,47 @@ describe('Die', () => {
 
             expect(getByText('Invalid die face: WAT')).toBeTruthy();
         });
-    })
+    });
+
+    describe('Dragging', () => {
+        it('should call onDragStart if die has onDragStart and id', () => {
+            expectedDieProps.id = '0';
+            expectedDieProps.onDragStart = mockOnDragStart;
+            expectedDieProps.color = dieColor.BLUE;
+            expectedDieProps.draggable = true;
+
+            const { getByTestId } = render(<Die {...expectedDieProps} />);
+            const die = getByTestId('BlueDie');
+
+            fireEvent.dragStart(die);
+
+            expect(mockOnDragStartCalled).toBe(true);
+        });
+
+        it('should not call onDragStart if die has no id', () => {
+            expectedDieProps.onDragStart = mockOnDragStart;
+            expectedDieProps.color = dieColor.BLUE;
+            expectedDieProps.draggable = true;
+
+            const { getByTestId } = render(<Die {...expectedDieProps} />);
+            const die = getByTestId('BlueDie');
+
+            fireEvent.dragStart(die);
+
+            expect(mockOnDragStartCalled).toBe(false);
+        });
+
+        it('should not call onDragStart if die has onDragMethod', () => {
+            expectedDieProps.id = '0';
+            expectedDieProps.color = dieColor.BLUE;
+            expectedDieProps.draggable = true;
+
+            const { getByTestId } = render(<Die {...expectedDieProps} />);
+            const die = getByTestId('BlueDie');
+
+            fireEvent.dragStart(die);
+
+            expect(mockOnDragStartCalled).toBe(false);
+        });
+    });
 });
