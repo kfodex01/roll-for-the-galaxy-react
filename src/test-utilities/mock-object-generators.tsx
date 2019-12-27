ebcfc5c3cf78b5ffa02f3e8111af2cf2bf116efd
kfode <kfode@sourceallies.com>
2019-12-27 13:45:58 -0600

    Removing unused mocks.

--- src/test-utilities/mock-object-generators.tsx
+++ src/test-utilities/mock-object-generators.tsx
@@ -2,7 +2,6 @@ import Chance from 'chance';
 import { DieProps } from '../components/Die';
 import { dieColor, dieFace, tileTypes } from '../enums';
 import { TileProps } from '../components/Tile';
-import { Tiles } from '../components/ConstructionZone';
 
 const chance = new Chance();
 
@@ -28,17 +27,6 @@ export const getMockDie = (): DieProps => {
     }
 };
 
-export const getArrayOfRandomDice = (): Array<DieProps> => {
-    let numberOfDice: number = chance.d10();
-    let result: Array<DieProps> = [];
-
-    for(let i: number = 0; i < numberOfDice; i++) {
-        result.push(getMockDie());
-    };
-
-    return result;
-}
-
 export const getMockTile = (name: string = chance.word(), points: number = chance.integer({ min: 1, max: 100 })): TileProps => {
     return {
         name,
@@ -53,24 +41,3 @@ export const getMockTile = (name: string = chance.word(), points: number = chanc
         ])
     }
 };
-
-export const getMockFullTile = (names: Array<string> = [chance.word(), chance.word()], points: Array<number> = [chance.integer({ min: 1, max: 100 }), chance.integer({ min: 1, max: 100 })]): Tiles => {
-    return {
-        tileId: chance.integer({ min: 1, max: 100 }),
-        tiles: [
-            getMockTile(names[0], points[0]),
-            getMockTile(names[1], points[1])
-        ]
-    };
-};
-
-export const getArrayOfRandomTiles = (): Array<Tiles> => {
-    let numberOfTiles: number = chance.d10();
-    let result: Array<Tiles> = [];
-
-    for(let i: number = 0; i < numberOfTiles; i++) {
-        result.push(getMockFullTile());
-    };
-
-    return result;
-}
